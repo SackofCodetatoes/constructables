44acaffd6c01be3cec30fc401537959e9a855a2a
Jason Wong <jsnwong@ucdavis.edu>
2018-08-08 11:36:10 -0700

    Create session_api_util file

--- frontend/constructables.jsx
+++ frontend/constructables.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import * as SessionAPI from './util/session_api_util';
 
 
 document.addEventListener('DOMContentLoaded', () => {
   const root = document.getElementById('root');
+
+  window.login = SessionAPI.login;
+  window.signup = SessionAPI.signup;
+  window.logout = SessionAPI.logout;
+
   ReactDOM.render(<h1>Welcome to Constructables</h1>, root);
 });
