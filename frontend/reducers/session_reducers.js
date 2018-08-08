e0e02a0824df69e77524e8b8b5273b6b51342f0f
Jason Wong <jsnwong@ucdavis.edu>
2018-08-08 12:17:04 -0700

    Start sessionReducer

---
+++ frontend/reducers/session_reducers.js
@@ -0,0 +1,10 @@
+import { RECEIVE_CURRENT_USER, LOGOUT_CURRENT_USER } from '../actions/session_actions';
+
+const _nullSession = { id: null };
+
+const sessionReducer = () => {
+
+
+}
+
+export default sessionReducer;
