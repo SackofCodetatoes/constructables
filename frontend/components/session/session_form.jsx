a5e8c1fa9a20712c106d50ffa18bc62d1c5c92b7
Jason Wong <jsnwong@ucdavis.edu>
2018-08-09 17:41:30 -0700

    Add styling to form session fields

--- frontend/components/session/session_form.jsx
+++ frontend/components/session/session_form.jsx
@@ -41,28 +41,37 @@ class SessionForm extends React.Component {
     
     const displayLogin = () => (
       <div>
-        <h1>Login</h1>
-        <input type="text" onChange={this.update('username')} value={this.state.username} placeholder="Username" />
-        <input type="password" onChange={this.update('password')} value={this.state.password} placeholder="Password" />
+        <input className='session-field' type="text" onChange={this.update('username')} value={this.state.username} placeholder="Username" /><br/>
+        <input className='session-field' type="password" onChange={this.update('password')} value={this.state.password} placeholder="Password" />
       </div>
     );
     
     const displaySignup = () => (
       <div>
         <h1>Sign Me Up!</h1>
-        <input type="text" onChange={this.update('email')} value={this.state.email} placeholder="Email" />
+        <input className='text-field' type="text" onChange={this.update('email')} value={this.state.email} placeholder="Email" />
         <input type="text" onChange={this.update('username')} value={this.state.username} placeholder="Username" />
         <input type="password" onChange={this.update('password')} value={this.state.password} placeholder="Password" />
         <input type="text" onChange={this.update('age')} value={this.state.age} placeholder="Age (dd/mm/yyyy)" />
       </div>
     );
 
+    let displayForm;
+    let buttonText;
+    if(this.props.formType === 'login') {
+      displayForm = displayLogin;
+      buttonText = 'Login';
+    } else {
+      displayForm = displaySignup;
+      buttonText = 'Sign Me Up!';
+    } 
+
     return (
-      <div>
+      <div className='form-container'>
         <form onSubmit={this.handleSubmit }>
-          { this.props.formType === 'login' ? displayLogin() : displaySignup() }
+          { displayForm() }
           { this.renderErrors() }
-          <input type="submit" value="Do Something"/>
+          <input className= 'form-submit-button' type="submit" value={buttonText}/>
         </form>
       </div>
     );
