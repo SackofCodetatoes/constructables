fe419f925e554863716948cbdcefefd83d846f59
Jason Wong <jsnwong@ucdavis.edu>
2018-08-11 21:16:00 -0700

    Start active storage tutorial and create projects table

---
+++ app/controllers/project_controller.rb
@@ -0,0 +1,22 @@
+class ProjectController < ApplicationController
+  def create
+  end
+
+  def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  get 'project/create'
+  get 'project/index'
+  get 'project/show'
+  get 'project/new'
+  get 'project/edit'
+  get 'project/update'
+  get 'project/destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api, defaults: {format: :json} do 
     resources :users, only: [:create, :show, :update, :edit]
