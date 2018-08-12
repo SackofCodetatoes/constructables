e1dc6db6704df7e03988504d5a076f31800105b3
Jason Wong <jsnwong@ucdavis.edu>
2018-08-11 22:10:07 -0700

    create project json builder partial

--- app/controllers/api/projects_controller.rb
+++ app/controllers/api/projects_controller.rb
@@ -1,4 +1,4 @@
-class ProjectController < ApplicationController
+class Api::ProjectsController < ApplicationController
   def create
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,9 @@
 Rails.application.routes.draw do
-  get 'project/create'
-  get 'project/index'
-  get 'project/show'
-  get 'project/new'
-  get 'project/edit'
-  get 'project/update'
-  get 'project/destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api, defaults: {format: :json} do 
     resources :users, only: [:create, :show, :update, :edit]
     resource :session, only: [:create, :destroy]
+    resources :projects, only: [:create, :update, :show, :index, :destroy]
   end
   root "static_pages#root"
 end
