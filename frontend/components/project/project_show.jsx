d4276257341593c193991d026264f89472e60fe3
Jason Wong <jsnwong@ucdavis.edu>
2018-08-13 22:17:29 -0700

    Temporarily change page logo and attempt to fix refresh project show error

--- frontend/components/project/project_show.jsx
+++ frontend/components/project/project_show.jsx
@@ -2,18 +2,27 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 class ProjectShow extends React.Component {
   componentDidMount(){
-    this.props.fetchPost(this.props.match.params.postId);
+    this.props.fetchProject(this.props.match.params.projectId);
+  }
+  componentWillReceiveProps(nextProps){
+    if(this.props.project.id = nextProps.match.params.projectId) {
+      this.props.fetchProject(nextProps.match.params.projectId);
+    }
   }
 
   render() {
     console.log('props');
     console.log(this.props);
+    const { project } = this.props;
+    if(!project) {
+      return (<div>Uhhh, gimmie a sec...</div>);
+    }
     return (
       <div>
         <ul>
-nothing
+          <li>{project.title}</li>
+          <li>{project.description}</li>
         </ul>
-        
       </div>
     )
   }
