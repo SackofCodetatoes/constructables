c64a0f2cd5484d7472919a9f3ef006d26f01693d
Jason Wong <jsnwong@ucdavis.edu>
2018-08-14 21:08:02 -0700

    Add functioning delete button on project show page

--- frontend/components/project/project_show.jsx
+++ frontend/components/project/project_show.jsx
@@ -11,13 +11,22 @@ class ProjectShow extends React.Component {
 
   render() {
     // console.log('props');
-    console.log(this.props);
+    // console.log(this.props.project);
     const { project } = this.props;
     const { user } = this.props;
 
-    const renderProjectDelete = () => {
+    const renderProjectOptions = () => {
       if(this.props.currentUserId === project.user_id ) {
-        return (<button className='project-delete-button'>Delete Project</button>)
+        return (
+          <div className="project-options">
+            <button onClick={() => this.props.deleteProject(project.id)} className='project-option-button project-delete-button'>
+              Delete Project
+            </button>
+            <button className='project-option-button'>
+              Edit
+            </button>
+          </div>
+        )
       } else return null;
     }
 
@@ -34,7 +43,7 @@ class ProjectShow extends React.Component {
             <li className='project-descripton'>Description: {project.description}</li>
           </ul>
         </div>
-        { renderProjectDelete() }
+        { renderProjectOptions() }
       </div>
     )
   }
